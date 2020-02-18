acb213f38fd0c57fcd9d740569b66f1717b8dc0c
Cesar Murillo <namiayumis@gmail.com>
2020-02-18 17:02:59 +0900

    agregando form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Formulario = () => {
-  return <h1>Formulario aqui</h1>;
+  return (
+    <form className="col-12">
+      <fieldset>
+        <legend>Busca bebidas por categoria o ingrediente</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            type="text"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select className="form-control" name="categoria">
+            <option value="">-- Selecciona Categoría --</option>
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
