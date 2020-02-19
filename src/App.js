98d355b884efc99c43c0261b367bbdb76e869e7b
Cesar Murillo <namiayumis@gmail.com>
2020-02-19 21:58:40 +0900

    mostrando las recetas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import CategoriasProvider from "./context/CategoriasContext";
 import RecetasProvider from "./context/RecetasContext";
+import ListaRecetas from "./components/ListaRecetas";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <div className="row">
             <Formulario />
           </div>
+          <ListaRecetas />
         </div>
       </RecetasProvider>
     </CategoriasProvider>
