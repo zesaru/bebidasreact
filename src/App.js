5fcd95c1ed6f950b3ccc2798caec63a0419bdc01
Cesar Murillo <namiayumis@gmail.com>
2020-02-19 10:37:53 +0900

    creando el context de categorias

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
-
+import CategoriasProvider from "./context/CategoriasContext";
 function App() {
   return (
-    <>
+    <CategoriasProvider>
       <Header />
       <div className="container mt-5">
         <div className="row">
           <Formulario />
         </div>
       </div>
-    </>
+    </CategoriasProvider>
   );
 }
 
